41cf6bf06f4bd9dd6089498536eb27f069f3aae8
Juan <107631551+JuanFelipe79@users.noreply.github.com>
2024-08-23 20:44:27 -0300

    Add files via upload

---
+++ aula4/randomico.java
@@ -0,0 +1,13 @@
+class randomico
+{
+    public static void main(String args[])
+    {
+        double x;
+        x = 0;
+        while(x < 3)
+        {
+            x = Math.random()*7;
+        }
+        System.out.println("Resultado: " + x);
+    }
+}
